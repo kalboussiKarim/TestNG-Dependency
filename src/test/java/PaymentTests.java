1b972cb48f295b33fe9120f0de5cd30249e318be
kalboussi karim <kalboussikarim3@gmail.com>
2025-01-02 21:27:54 +0100

    added include & exclude tags in the test groups tag

--- src/test/java/PaymentTests.java
+++ src/test/java/PaymentTests.java
@@ -2,12 +2,12 @@ import org.testng.annotations.Test;
 
 public class PaymentTests {
 
-    @Test(priority = 1,groups = {"regression","sanity"})
+    @Test(priority = 1,groups = {"regression","sanity","functional"})
     void paymentInDinar(){
         System.out.println("This is payment in dinars...");
     }
 
-    @Test(priority = 2,groups = {"regression","sanity"})
+    @Test(priority = 2,groups = {"regression","sanity","functional"})
     void paymentInEuros(){
         System.out.println("This is payment in dollars...");
     }
